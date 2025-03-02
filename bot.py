bdf6205183c5df260c8ef6416de276f8cc62da56
IlikoN-debug <ikirik229@gmail.com>
2025-03-02 13:29:11 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -85,12 +85,16 @@ def main():
     application.add_handler(CommandHandler("start", start))
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
     
+    # Указываем порт и хост для Render
+    port = int(os.environ.get("PORT", 5000))
+    host = os.environ.get("RENDER_EXTERNAL_HOSTNAME", "0.0.0.0")
+    
+    # Запуск вебхука
     application.run_webhook(
-        listen="0.0.0.0",
-        port=int(os.environ.get("PORT", 5000)),
+        listen=host,
+        port=port,
         url_path=TOKEN,
-        webhook_url=f"https://{os.environ.get('RENDER_EXTERNAL_HOSTNAME')}/{TOKEN}"
+        webhook_url=f"https://{host}/{TOKEN}"
     )
-
 if __name__ == "__main__":
     main()
